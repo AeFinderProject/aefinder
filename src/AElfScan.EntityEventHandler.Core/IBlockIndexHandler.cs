70b57c2beb31315709d943eb147fbd98fb4c3c41
zhxymh <sean.zhang@aelf.io>
2022-11-09 15:58:20 +0800

    fix: fix scope problem

--- src/AElfScan.EntityEventHandler.Core/IBlockIndexHandler.cs
+++ src/AElfScan.EntityEventHandler.Core/IBlockIndexHandler.cs
@@ -6,6 +6,8 @@ using AElfScan.Grains.Grain.BlockScan;
 using AElfScan.Grains.Grain.Chains;
 using AElfScan.Orleans;
 using AElfScan.Orleans.EventSourcing.Grain.BlockScan;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Nito.AsyncEx;
 using Orleans;
 using Volo.Abp.DependencyInjection;
@@ -16,66 +18,88 @@ namespace AElfScan;
 public interface IBlockIndexHandler
 {
     Task ProcessNewBlockAsync(BlockIndex block);
-    Task ProcessConfirmBlocksAsync(List<BlockIndex> confirmBlocks);
+    Task ProcessConfirmedBlocksAsync(List<BlockIndex> confirmBlocks);
 }
 
-public class BlockIndexHandler : IBlockIndexHandler, ITransientDependency
+public class BlockIndexHandler : IBlockIndexHandler, ISingletonDependency
 {
     private readonly IClusterClient _clusterClient;
     private readonly IObjectMapper _objectMapper;
+    
+    public ILogger<BlockIndexHandler> Logger { get; set; }
 
     public BlockIndexHandler(IObjectMapper objectMapper, IClusterClient clusterClient)
     {
         _objectMapper = objectMapper;
         _clusterClient = clusterClient;
+        
+        Logger = NullLogger<BlockIndexHandler>.Instance;
     }
 
     public async Task ProcessNewBlockAsync(BlockIndex block)
     {
-        var chainGrain = _clusterClient.GetGrain<IChainGrain>(block.ChainId);
-        await chainGrain.SetLatestBlockAsync(block.BlockHash, block.BlockNumber);
-
-        var clientManagerGrain = _clusterClient.GetGrain<IClientManagerGrain>(0);
-        var clientIds = await clientManagerGrain.GetClientIdsByChainAsync(block.ChainId);
-        var tasks = clientIds.Select(async clientId =>
+        try
         {
-            var clientGrain = _clusterClient.GetGrain<IClientGrain>(clientId);
-            var clientInfo = await clientGrain.GetClientInfoAsync();
-            if (clientInfo.ScanModeInfo.ScanMode == ScanMode.NewBlock &&
-                clientInfo.ScanModeInfo.ScanNewBlockStartHeight <= block.BlockNumber)
-            {
-                var blockScanGrain = _clusterClient.GetGrain<IBlockScanGrain>(clientId);
-                var dto = _objectMapper.Map<BlockIndex, BlockDto>(block);
-                await blockScanGrain.HandleNewBlockAsync(dto);
-            }
-        });
+            var chainGrain = _clusterClient.GetGrain<IChainGrain>(block.ChainId);
+            await chainGrain.SetLatestBlockAsync(block.BlockHash, block.BlockNumber);
+            
+            var dto = _objectMapper.Map<BlockIndex, BlockDto>(block);
 
-        await tasks.WhenAll();
+            var clientManagerGrain = _clusterClient.GetGrain<IClientManagerGrain>(0);
+            var clientIds = await clientManagerGrain.GetClientIdsByChainAsync(block.ChainId);
+            var tasks = clientIds.Select(async clientId =>
+            {
+                var clientGrain = _clusterClient.GetGrain<IClientGrain>(clientId);
+                var clientInfo = await clientGrain.GetClientInfoAsync();
+                if (clientInfo.ScanModeInfo.ScanMode == ScanMode.NewBlock &&
+                    clientInfo.ScanModeInfo.ScanNewBlockStartHeight <= block.BlockNumber)
+                {
+                    var blockScanGrain = _clusterClient.GetGrain<IBlockScanGrain>(clientId);
+                    await blockScanGrain.HandleNewBlockAsync(dto);
+                }
+            });
+
+            await tasks.WhenAll();
+        }
+        catch (Exception e)
+        {
+            Logger.LogError(e,$"Process new block failed.");
+            throw;
+        }
     }
 
-    public async Task ProcessConfirmBlocksAsync(List<BlockIndex> confirmBlocks)
+    public async Task ProcessConfirmedBlocksAsync(List<BlockIndex> confirmBlocks)
     {
-        var chainId = confirmBlocks.First().ChainId;
-
-        var chainGrain = _clusterClient.GetGrain<IChainGrain>(chainId);
-        await chainGrain.SetLatestConfirmBlockAsync(confirmBlocks.Last().BlockHash,
-            confirmBlocks.Last().BlockNumber);
-
-        var clientManagerGrain = _clusterClient.GetGrain<IClientManagerGrain>(0);
-        var clientIds = await clientManagerGrain.GetClientIdsByChainAsync(chainId);
-        var tasks = clientIds.Select(async clientId =>
+        try
         {
-            var clientGrain = _clusterClient.GetGrain<IClientGrain>(clientId);
-            var clientInfo = await clientGrain.GetClientInfoAsync();
-            if (clientInfo.ScanModeInfo.ScanMode == ScanMode.NewBlock &&
-                clientInfo.ScanModeInfo.ScanNewBlockStartHeight <= confirmBlocks.First().BlockNumber)
-            {
-                var blockScanGrain = _clusterClient.GetGrain<IBlockScanGrain>(clientId);
-                var dtos = _objectMapper.Map<List<BlockIndex>, List<BlockDto>>(confirmBlocks);
-                await blockScanGrain.HandleConfirmedBlockAsync(dtos);
-            }
-        });
+            var chainId = confirmBlocks.First().ChainId;
 
-        await tasks.WhenAll();
+            var chainGrain = _clusterClient.GetGrain<IChainGrain>(chainId);
+            await chainGrain.SetLatestConfirmBlockAsync(confirmBlocks.Last().BlockHash,
+                confirmBlocks.Last().BlockNumber);
+
+            var dtos = _objectMapper.Map<List<BlockIndex>, List<BlockDto>>(confirmBlocks);
+
+            var clientManagerGrain = _clusterClient.GetGrain<IClientManagerGrain>(0);
+            var clientIds = await clientManagerGrain.GetClientIdsByChainAsync(chainId);
+            var tasks = clientIds.Select(async clientId =>
+            {
+                var clientGrain = _clusterClient.GetGrain<IClientGrain>(clientId);
+                var clientInfo = await clientGrain.GetClientInfoAsync();
+                if (clientInfo.ScanModeInfo.ScanMode == ScanMode.NewBlock &&
+                    clientInfo.ScanModeInfo.ScanNewBlockStartHeight <= confirmBlocks.First().BlockNumber)
+                {
+                    var blockScanGrain = _clusterClient.GetGrain<IBlockScanGrain>(clientId);
+                    await blockScanGrain.HandleConfirmedBlockAsync(dtos);
+                }
+            });
+
+            await tasks.WhenAll();
+        }
+        catch (Exception e)
+        {
+            Logger.LogError(e,$"Process Confirmed block failed.");
+            throw;
+        }
     }
 }
