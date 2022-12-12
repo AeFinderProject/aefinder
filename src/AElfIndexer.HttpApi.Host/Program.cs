1e68e58f5ca026e2b8a51ac1be0d16bcbe00b78a
zhxymh <sean.zhang@aelf.io>
2022-12-12 14:54:16 +0800

    test: temp disable plugin

--- src/AElfIndexer.HttpApi.Host/Program.cs
+++ src/AElfIndexer.HttpApi.Host/Program.cs
@@ -33,10 +33,10 @@ public class Program
             builder.Host.AddAppSettingsSecretsJson()
                 .UseAutofac()
                 .UseSerilog();
-            await builder.AddApplicationAsync<AElfIndexerHttpApiHostModule>(options =>
-            {
-                options.PlugInSources.AddFolder(builder.Configuration.GetSection("PlugIns")["Path"]);
-            });
+            // await builder.AddApplicationAsync<AElfIndexerHttpApiHostModule>(options =>
+            // {
+            //     options.PlugInSources.AddFolder(builder.Configuration.GetSection("PlugIns")["Path"]);
+            // });
             var app = builder.Build();
             await app.InitializeApplicationAsync();
             await app.RunAsync();
