adcc1ad4257c74ffee2b87626837d865357f877b
zhxymh <sean.zhang@aelf.io>
2022-12-13 14:31:39 +0800

    test: add client grain tests

--- test/AElfIndexer.Grains.Tests/BlockScan/ClientGrainTests.cs
+++ test/AElfIndexer.Grains.Tests/BlockScan/ClientGrainTests.cs
@@ -1,8 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using AElfIndexer.Block.Dtos;
 using AElfIndexer.BlockScan;
 using AElfIndexer.Grains.Grain.BlockScan;
 using AElfIndexer.Grains.State.BlockScan;
@@ -15,80 +14,112 @@ namespace AElfIndexer.Grains.BlockScan;
 public class ClientGrainTests : AElfIndexerGrainTestBase
 {
     [Fact]
-    public async Task InitializeTest()
+    public async Task ClientTest()
     {
-        var chainId = "AELF";
-        var clientId = "DApp";
-        var version = "Version";
-        var subscriptionInfo = new SubscriptionInfo
+        var clientId = "client";
+        var subscriptionInfo1 = new List<SubscriptionInfo>
         {
-            ChainId = chainId,
-            OnlyConfirmedBlock = true,
-            StartBlockNumber = 100,
-            SubscribeEvents = new List<FilterContractEventInput>
+            new SubscriptionInfo
             {
-                new FilterContractEventInput
-                {
-                    ContractAddress = "ContractAddress",
-                    EventNames = new List<string>{"EventName"}
-                }
+                ChainId = "AELF",
+                FilterType = BlockFilterType.Block,
             }
         };
         
-        var clientGrain = Cluster.Client.GetGrain<IBlockScanInfoGrain>(clientId);
-        await clientGrain.InitializeAsync(chainId, clientId,  version,subscriptionInfo);
-        var clientInfo = await clientGrain.GetClientInfoAsync();
-        clientInfo.ChainId.ShouldBe(chainId);
-        clientInfo.ClientId.ShouldBe(clientId);
-        clientInfo.ScanModeInfo.ScanMode.ShouldBe(ScanMode.HistoricalBlock);
-        clientInfo.ScanModeInfo.ScanNewBlockStartHeight.ShouldBe(0);
+        var clientGrain = Cluster.Client.GetGrain<IClientGrain>(clientId);
+        var version1 = await clientGrain.AddSubscriptionInfoAsync(subscriptionInfo1);
 
-        var subscribe = await clientGrain.GetSubscriptionInfoAsync();
-        subscribe.ChainId.ShouldBe(subscriptionInfo.ChainId);
-        subscribe.OnlyConfirmedBlock.ShouldBe(subscriptionInfo.OnlyConfirmedBlock);
-        subscribe.StartBlockNumber.ShouldBe(subscriptionInfo.StartBlockNumber);
-        subscribe.SubscribeEvents.Count.ShouldBe(1);
-        subscribe.SubscribeEvents[0].ContractAddress.ShouldBe(subscriptionInfo.SubscribeEvents[0].ContractAddress);
-        subscribe.SubscribeEvents[0].EventNames.Count.ShouldBe(1);
-        subscribe.SubscribeEvents[0].EventNames[0].ShouldBe(subscriptionInfo.SubscribeEvents[0].EventNames[0]);
+        var subscription1 = await clientGrain.GetSubscriptionInfoAsync(version1);
+        subscription1.Count.ShouldBe(1);
+        subscription1[0].ChainId.ShouldBe("AELF");
+        subscription1[0].FilterType.ShouldBe(BlockFilterType.Block);
+        
+        var subscriptionInfo2 = new List<SubscriptionInfo>
+        {
+            new SubscriptionInfo
+            {
+                ChainId = "AELF",
+                FilterType = BlockFilterType.Transaction,
+            }
+        };
+        
+        var version2 = await clientGrain.AddSubscriptionInfoAsync(subscriptionInfo2);
 
-        var clientManagerGrain = Cluster.Client.GetGrain<IBlockScanManagerGrain>(0);
-        var clientIds = await clientManagerGrain.GetBlockScanIdsByChainAsync("AELF");
-        clientIds.Count.ShouldBe(1);
-        clientIds[0].ShouldBe(clientId);
+        var subscription2 = await clientGrain.GetSubscriptionInfoAsync(version2);
+        subscription2.Count.ShouldBe(1);
+        subscription2[0].ChainId.ShouldBe("AELF");
+        subscription2[0].FilterType.ShouldBe(BlockFilterType.Transaction);
+        
+        var isAvailable = await clientGrain.IsVersionAvailableAsync(version1);
+        isAvailable.ShouldBe(true);
+        isAvailable = await clientGrain.IsVersionAvailableAsync(version2);
+        isAvailable.ShouldBe(true);
+        isAvailable = await clientGrain.IsVersionAvailableAsync(Guid.NewGuid().ToString());
+        isAvailable.ShouldBe(false);
 
-        var allClientIds = await clientManagerGrain.GetAllBlockScanIdsAsync();
-        allClientIds.Keys.Count.ShouldBe(1);
-        allClientIds["AELF"].Count.ShouldBe(1);
-        allClientIds["AELF"].First().ShouldBe(clientId);
+        var version = await clientGrain.GetVersionAsync();
+        version.CurrentVersion.ShouldBe(version1);
+        version.NewVersion.ShouldBe(version2);
 
-        await clientGrain.SetScanNewBlockStartHeightAsync(80);
-        clientInfo = await clientGrain.GetClientInfoAsync();
-        clientInfo.ScanModeInfo.ScanMode.ShouldBe(ScanMode.NewBlock);
-        clientInfo.ScanModeInfo.ScanNewBlockStartHeight.ShouldBe(80);
+        var versionStatus = await clientGrain.GetVersionStatusAsync(version1);
+        versionStatus.ShouldBe(VersionStatus.Initialized);
+        versionStatus = await clientGrain.GetVersionStatusAsync(version2);
+        versionStatus.ShouldBe(VersionStatus.Initialized);
 
-        // await clientGrain.StopAsync(Guid.NewGuid().ToString());
-        // clientInfo = await clientGrain.GetClientInfoAsync();
-        // clientInfo.Version.ShouldBe(version);
+        await clientGrain.StartAsync(version1);
+        versionStatus = await clientGrain.GetVersionStatusAsync(version1);
+        versionStatus.ShouldBe(VersionStatus.Started);
+
+        await clientGrain.UpgradeVersionAsync();
+        version = await clientGrain.GetVersionAsync();
+        version.CurrentVersion.ShouldBe(version2);
+        version.NewVersion.ShouldBeNull();
+    }
+
+    [Fact]
+    public async Task RemoveVersionInfoTest()
+    {
+        var clientId = "client";
+        var subscriptionInfo1 = new List<SubscriptionInfo>
+        {
+            new SubscriptionInfo
+            {
+                ChainId = "AELF",
+                FilterType = BlockFilterType.Block,
+            }
+        };
         
-        clientIds = await clientManagerGrain.GetBlockScanIdsByChainAsync("AELF");
-        clientIds.Count.ShouldBe(1);
-        clientIds[0].ShouldBe(clientId);
+        var clientGrain = Cluster.Client.GetGrain<IClientGrain>(clientId);
+        var version1 = await clientGrain.AddSubscriptionInfoAsync(subscriptionInfo1);
+
+        var subscriptionInfo2 = new List<SubscriptionInfo>
+        {
+            new SubscriptionInfo
+            {
+                ChainId = "AELF",
+                FilterType = BlockFilterType.Transaction,
+            }
+        };
         
-        allClientIds = await clientManagerGrain.GetAllBlockScanIdsAsync();
-        allClientIds.Keys.Count.ShouldBe(1);
-        allClientIds["AELF"].Count.ShouldBe(1);
-        allClientIds["AELF"].First().ShouldBe(clientId);
+        var version2 = await clientGrain.AddSubscriptionInfoAsync(subscriptionInfo2);
+
+        await clientGrain.AddBlockScanIdAsync(version1, "id1");
+        await clientGrain.AddBlockScanIdAsync(version2, "id2");
+
+        var ids = await clientGrain.GetBlockScanIdsAsync(version1);
+        ids.Count.ShouldBe(1);
+        ids[0].ShouldBe("id1");
         
-        // await clientGrain.StopAsync(version);
-        // clientInfo = await clientGrain.GetClientInfoAsync();
-        // clientInfo.Version.ShouldNotBe(version);
+        ids = await clientGrain.GetBlockScanIdsAsync(version2);
+        ids.Count.ShouldBe(1);
+        ids[0].ShouldBe("id2");
+
+        await clientGrain.RemoveVersionInfoAsync(version1);
+        ids = await clientGrain.GetBlockScanIdsAsync(version1);
+        ids.Count.ShouldBe(1);
         
-        clientIds = await clientManagerGrain.GetBlockScanIdsByChainAsync("AELF");
-        clientIds.Count.ShouldBe(0);
-        
-        allClientIds = await clientManagerGrain.GetAllBlockScanIdsAsync();
-        allClientIds.Keys.Count.ShouldBe(1);
-        allClientIds["AELF"].Count.ShouldBe(0);
+        await clientGrain.RemoveVersionInfoAsync(version2);
+        ids = await clientGrain.GetBlockScanIdsAsync(version2);
+        ids.Count.ShouldBe(0);
     }
 }
