651d94f54878f6b5cc905463efe34209783f8776
Bourne Shi <bourne.shi@aelf.io>
2022-12-26 16:23:07 +0800

    feat: fix abp 7.0 dependency

--- src/AElfIndexer.BlockChainEventHandler/AElfIndexerBlockChainEventHandlerModule.cs
+++ src/AElfIndexer.BlockChainEventHandler/AElfIndexerBlockChainEventHandlerModule.cs
@@ -5,12 +5,16 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Orleans.Providers.MongoDB.Configuration;
 using Volo.Abp;
 using Volo.Abp.AspNetCore.Serilog;
 using Volo.Abp.Autofac;
 using Volo.Abp.EventBus.Distributed;
 using Volo.Abp.EventBus.RabbitMq;
 using Volo.Abp.Modularity;
+using Volo.Abp.Threading;
 
 namespace AElfIndexer;
 
@@ -24,14 +28,54 @@ public class AElfIndexerBlockChainEventHandlerModule:AbpModule
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
         var configuration = context.Services.GetConfiguration();
-        // context.Services.AddHostedService<AElfIndexerClusterClientHostedService>();
         Configure<BlockChainEventHandlerOptions>(configuration.GetSection("BlockChainEventHandler"));
         
-        context.Services.AddSingleton<AElfIndexerClusterClientHostedService>();
-        context.Services.AddSingleton<IHostedService>(sp => sp.GetService<AElfIndexerClusterClientHostedService>());
-        context.Services.AddSingleton<IClusterClient>(sp => sp.GetService<AElfIndexerClusterClientHostedService>().OrleansClient);
-        context.Services.AddSingleton<IGrainFactory>(sp =>
-            sp.GetService<AElfIndexerClusterClientHostedService>().OrleansClient);
+        context.Services.AddHostedService<AElfIndexerClusterClientHostedService>();
+
+        // context.Services.AddSingleton<AElfIndexerClusterClientHostedService>();
+        // context.Services.AddSingleton<IHostedService>(sp => sp.GetService<AElfIndexerClusterClientHostedService>());
+        // context.Services.AddSingleton<IClusterClient>(sp => sp.GetService<AElfIndexerClusterClientHostedService>().OrleansClient);
+        // context.Services.AddSingleton<IGrainFactory>(sp =>
+        //     sp.GetService<AElfIndexerClusterClientHostedService>().OrleansClient);
+        
+        context.Services.AddSingleton<IClusterClient>(o =>
+        {
+            return new ClientBuilder()
+                .ConfigureDefaults()
+                // .UseRedisClustering(opt =>
+                // {
+                //     opt.ConnectionString = configuration["Orleans:ClusterDbConnection"];
+                //     opt.Database = Convert.ToInt32(configuration["Orleans:ClusterDbNumber"]);
+                // })
+                .UseMongoDBClient(configuration["Orleans:MongoDBClient"])
+                .UseMongoDBClustering(options =>
+                {
+                    options.DatabaseName = configuration["Orleans:DataBase"];;
+                    options.Strategy = MongoDBMembershipStrategy.SingleDocument;
+                })
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = configuration["Orleans:ClusterId"];
+                    options.ServiceId = configuration["Orleans:ServiceId"];
+                })
+                .ConfigureApplicationParts(parts =>
+                    parts.AddApplicationPart(typeof(AElfIndexerGrainsModule).Assembly).WithReferences())
+                .ConfigureLogging(builder => builder.AddProvider(o.GetService<ILoggerProvider>()))
+                .Build();
+        });
     }
+    
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        var client = context.ServiceProvider.GetRequiredService<IClusterClient>();
+        AsyncHelper.RunSync(async ()=> await client.Connect());
+    }
+
+    public override void OnApplicationShutdown(ApplicationShutdownContext context)
+    {
+        var client = context.ServiceProvider.GetRequiredService<IClusterClient>();
+        AsyncHelper.RunSync(client.Close);
+    }
+
     
 }
