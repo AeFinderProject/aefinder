8b1e52687800f56054879d38273bc71db5d8430e
Bourne Shi <bourne.shi@aelf.io>
2022-12-29 14:46:55 +0800

    feat: disable token cleanup service in silo

--- src/AElfIndexer.Silo/AElfIndexerSiloModule.cs
+++ src/AElfIndexer.Silo/AElfIndexerSiloModule.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.AspNetCore.Serilog;
 using Volo.Abp.Autofac;
 using Volo.Abp.Modularity;
+using Volo.Abp.OpenIddict.Tokens;
 
 namespace AElfIndexer;
 
@@ -15,5 +16,12 @@ public class AElfIndexerOrleansSiloModule:AbpModule
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
         context.Services.AddHostedService<AElfIndexerHostedService>();
+        ConfigureTokenCleanupService();
+    }
+    
+    //Disable TokenCleanupService
+    private void ConfigureTokenCleanupService()
+    {
+        Configure<TokenCleanupOptions>(x => x.IsCleanupEnabled = false);
     }
 }
