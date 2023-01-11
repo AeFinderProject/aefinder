63891575053fae5d80d8efcc07188067976ed049
zhxymh <sean.zhang@aelf.io>
2023-01-11 10:51:19 +0800

    test: add client test project

---
+++ test/AElfIndexer.Client.Tests/AElfIndexerClientTestModule.cs
@@ -0,0 +1,19 @@
+﻿using AElfIndexer.Client;
+using AElfIndexer.Orleans.TestBase;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute.Extensions;
+using Volo.Abp.Modularity;
+
+namespace AElfIndexer;
+
+[DependsOn(
+    typeof(AElfIndexerClientModule),
+    typeof(AElfIndexerDomainTestModule),
+    typeof(AElfIndexerOrleansTestBaseModule)
+    )]
+public class AElfIndexerClientTestModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+    }
+}
