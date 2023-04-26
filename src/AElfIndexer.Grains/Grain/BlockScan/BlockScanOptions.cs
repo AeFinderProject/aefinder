00ca96170e4f1e9232edfa6c12777013954fa935
zhxymh <sean.zhang@aelf.io>
2023-04-26 22:22:37 +0800

    fix: add push new block count

--- src/AElfIndexer.Grains/Grain/BlockScan/BlockScanOptions.cs
+++ src/AElfIndexer.Grains/Grain/BlockScan/BlockScanOptions.cs
@@ -5,4 +5,6 @@ public class BlockScanOptions
     public int BatchPushBlockCount { get; set; } = 100;
     public int ScanHistoryBlockThreshold { get; set; } = 1000;
     public int HistoricalPushRecoveryThreshold { get; set; } = 5;
+
+    public int PushNewBlockCount { get; set; } = 10;
 }
