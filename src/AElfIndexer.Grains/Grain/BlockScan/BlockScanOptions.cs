199da699cdca5ffd9fbc5d3c757730ca9c641d49
zhxymh <sean.zhang@aelf.io>
2023-09-25 14:20:16 +0800

    fix: scan option

--- src/AElfIndexer.Grains/Grain/BlockScan/BlockScanOptions.cs
+++ src/AElfIndexer.Grains/Grain/BlockScan/BlockScanOptions.cs
@@ -6,6 +6,6 @@ public class BlockScanOptions
     public int ScanHistoryBlockThreshold { get; set; } = 1000;
     public int HistoricalPushRecoveryThreshold { get; set; } = 5;
     public int BatchPushNewBlockCount { get; set; } = 10;
-    public int MaxHistoricalBlockPushThreshold = 10000;
-    public int MaxNewBlockPushThreshold = 5000;
+    public int MaxHistoricalBlockPushThreshold { get; set; } = 10000;
+    public int MaxNewBlockPushThreshold { get; set; }  = 5000;
 }
