a8ea8684ebef2379cc280357af40cfe6e44cfa0d
AeFinderBourne <astro@aefinder.io>
2024-03-21 11:49:53 +0800

    feat: add AeFinderKubernetesModule

---
+++ src/AeFinder.Kubernetes/AeFinderKubernetesModule.cs
@@ -0,0 +1,21 @@
+﻿using k8s;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Volo.Abp.Modularity;
+
+namespace AeFinder.Kubernetes;
+
+public class AeFinderKubernetesModule: AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var configuration = context.Services.GetConfiguration();
+        Configure<KubernetesOptions>(configuration.GetSection("Kubernetes"));
+        context.Services.AddSingleton<k8s.Kubernetes>(provider =>
+        {
+            var options = provider.GetRequiredService<IOptions<KubernetesOptions>>().Value;
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile(options.KubeConfigPath);
+            return new k8s.Kubernetes(config);
+        });
+    }
+}
---
+++ src/AeFinder.Kubernetes/KubernetesOptions.cs
@@ -0,0 +1,7 @@
+namespace AeFinder.Kubernetes;
+
+public class KubernetesOptions
+{
+    public string KubeConfigPath { get; set; } = "KubeConfig/config.txt";
+
+}
