25a57cb6f1141d7f0317a0289545db0450a44e19
kimbalSte <kimbal_s@aefinder.io>
2024-04-07 13:50:06 +0800

    fix: apollo ENV

--- src/AeFinder.Application.Contracts/AeFinderHostBuilderExtensions.cs
+++ src/AeFinder.Application.Contracts/AeFinderHostBuilderExtensions.cs
@@ -1,6 +1,9 @@
+using AElfIndexer;
 using Com.Ctrip.Framework.Apollo.Logging;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Prometheus;
 using Serilog;
 using Serilog.Events;
 
@@ -27,6 +30,19 @@ public static class AeFinderHostBuilderExtensions
         return hostBuilder
             .ConfigureAppConfiguration((_, builder) => { builder.AddApollo(builder.Build().GetSection("apollo")); })
             .ConfigureGloballySharedLog(false);
+    }
+
+    public static IHostBuilder AddMetrics(this IHostBuilder hostBuilder)
+    {
+        return hostBuilder.ConfigureServices((_, services) =>
+        {
+            services.AddMetricServer(options =>
+            {
+                var metricsOption = Commons.ConfigurationHelper.GetValue("Metrics", new MetricsOption());
+                options.Port = metricsOption.Port;
+            });
+            services.AddHealthChecks();
+        });
     }
 
     private static IHostBuilder AddAppSettingsApolloJson(this IHostBuilder hostBuilder)
