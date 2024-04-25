57d41da631eb039e959f0ef922a417e45c46881a
kimbal <kimbal_s@aefinder.io>
2024-04-25 22:23:25 +0800

    feat: modify app name regex

--- src/AeFinder.Application.Contracts/Studio/AppIdConsts.cs
+++ src/AeFinder.Application.Contracts/Studio/AppIdConsts.cs
@@ -3,7 +3,5 @@ namespace AeFinder.Studio;
 public static class AppIdConsts
 {
     public static int MaxNameLength { get; set; } = 30;
-    private const string QnameCharFmt = "[A-Za-z0-9]";
-    private const string QnameExtCharFmt = "[-A-Za-z0-9_.]";
-    public const string NameRegex = "(" + QnameCharFmt + QnameExtCharFmt + "*)?" + QnameCharFmt;
+    public const string NameRegex = "[a-z]([-a-z0-9]*[a-z0-9])?')";
 }
