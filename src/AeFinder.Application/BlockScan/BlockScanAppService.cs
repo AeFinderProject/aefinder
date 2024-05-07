79e2caaf0ac24fc5f97a23c67c5ef19487e2215d
kimbal <kimbal_s@aefinder.io>
2024-05-07 16:51:57 +0800

    fix: add log

--- src/AeFinder.Application/BlockScan/BlockScanAppService.cs
+++ src/AeFinder.Application/BlockScan/BlockScanAppService.cs
@@ -276,7 +276,9 @@ public class BlockScanAppService : AeFinderAppService, IBlockScanAppService
     public async Task StopAsync(string clientId, string version)
     {
         var clientGrain = _clusterClient.GetGrain<IAppSubscriptionGrain>(GrainIdHelper.GenerateAppSubscriptionGrainId(clientId));
+        Logger.LogDebug("ScanApp: {clientId} stop scan, version: {version}", clientId, version);
         await clientGrain.StopAsync(version);
+        Logger.LogDebug("ScanApp: {clientId} stop scan, version: {version}", clientId, version);
         await _kubernetesAppManager.DestroyAppPodAsync(clientId, version);
     }
 
