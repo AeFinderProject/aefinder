a3493cf5b36fae1c0d90c10d0f23b1477a363455
AeFinderBourne <astro@aefinder.io>
2024-05-08 19:43:48 +0800

    fix: depend on auto fac module

--- src/AeFinder.BackgroundWorker/AeFinderBackGroundModule.cs
+++ src/AeFinder.BackgroundWorker/AeFinderBackGroundModule.cs
@@ -2,13 +2,15 @@ using AeFinder.App.Deploy;
 using AeFinder.Kubernetes;
 using AeFinder.Kubernetes.Manager;
 using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Autofac;
 using Volo.Abp.AutoMapper;
 using Volo.Abp.EventBus.RabbitMq;
 using Volo.Abp.Modularity;
 
 namespace AeFinder.BackgroundWorker;
 
-[DependsOn(typeof(AeFinderKubernetesModule),
+[DependsOn(typeof(AbpAutofacModule),
+    typeof(AeFinderKubernetesModule),
     typeof(AbpEventBusRabbitMqModule),
     typeof(AeFinderApplicationModule))]
 public class AeFinderBackGroundModule : AbpModule
