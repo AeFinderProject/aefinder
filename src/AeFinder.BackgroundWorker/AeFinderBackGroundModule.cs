e59139f83cd88a30e84bb1e94ec69e31b6b7f222
AeFinderBourne <astro@aefinder.io>
2024-05-09 13:52:54 +0800

    fix: add orleans client config for background worker

--- src/AeFinder.BackgroundWorker/AeFinderBackGroundModule.cs
+++ src/AeFinder.BackgroundWorker/AeFinderBackGroundModule.cs
@@ -1,13 +1,22 @@
 using AeFinder.App.Deploy;
+using AeFinder.Grains;
 using AeFinder.Kubernetes;
 using AeFinder.Kubernetes.Manager;
 using AeFinder.MongoDb;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Providers.MongoDB.Configuration;
+using Volo.Abp;
 using Volo.Abp.Autofac;
 using Volo.Abp.AutoMapper;
+using Volo.Abp.Caching;
 using Volo.Abp.EventBus.RabbitMq;
 using Volo.Abp.Modularity;
 using Volo.Abp.OpenIddict.Tokens;
+using Volo.Abp.Threading;
 
 namespace AeFinder.BackgroundWorker;
 
@@ -20,16 +29,65 @@ public class AeFinderBackGroundModule : AbpModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
+        var configuration = context.Services.GetConfiguration();
         context.Services.AddHttpClient();
         context.Services.AddHostedService<AeFinderHostedService>();
         Configure<AbpAutoMapperOptions>(options => { options.AddMaps<AeFinderBackGroundModule>(); });
         context.Services.AddTransient<IAppDeployManager, KubernetesAppManager>();
         ConfigureTokenCleanupService();
+        
+        context.Services.AddSingleton<IClusterClient>(o =>
+        {
+            return new ClientBuilder()
+                .ConfigureDefaults()
+                // .UseRedisClustering(opt =>
+                // {
+                //     opt.ConnectionString = configuration["Orleans:ClusterDbConnection"];
+                //     opt.Database = Convert.ToInt32(configuration["Orleans:ClusterDbNumber"]);
+                // })
+                .UseMongoDBClient(configuration["Orleans:MongoDBClient"])
+                .UseMongoDBClustering(options =>
+                {
+                    options.DatabaseName = configuration["Orleans:DataBase"];;
+                    options.Strategy = MongoDBMembershipStrategy.SingleDocument;
+                })
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = configuration["Orleans:ClusterId"];
+                    options.ServiceId = configuration["Orleans:ServiceId"];
+                })
+                .Configure<ClientMessagingOptions>(options =>
+                {
+                    options.ResponseTimeout = TimeSpan.FromSeconds(Convert.ToInt32(configuration["Orleans:GrainResponseTimeOut"]));
+                    options.MaxMessageBodySize = Convert.ToInt32(configuration["Orleans:GrainMaxMessageBodySize"]);
+                })
+                .ConfigureApplicationParts(parts =>
+                    parts.AddApplicationPart(typeof(AeFinderGrainsModule).Assembly).WithReferences())
+                .ConfigureLogging(builder => builder.AddProvider(o.GetService<ILoggerProvider>()))
+                .Build();
+        });
     }
     
     //Disable TokenCleanupBackgroundWorker
     private void ConfigureTokenCleanupService()
     {
         Configure<TokenCleanupOptions>(x => x.IsCleanupEnabled = false);
+    }
+    
+    private void ConfigureCache(IConfiguration configuration)
+    {
+        Configure<AbpDistributedCacheOptions>(options => { options.KeyPrefix = "AeFinder:"; });
+    }
+    
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        var client = context.ServiceProvider.GetRequiredService<IClusterClient>();
+        AsyncHelper.RunSync(async ()=> await client.Connect());
+    }
+
+    public override void OnApplicationShutdown(ApplicationShutdownContext context)
+    {
+        var client = context.ServiceProvider.GetRequiredService<IClusterClient>();
+        AsyncHelper.RunSync(client.Close);
     }
 }
