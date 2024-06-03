f698ecdcabd49766888402917a0cad1e641165e5
AeFinderBourne <astro@aefinder.io>
2024-06-03 15:32:30 +0800

    feat: optimize organizaiton dto

--- src/AeFinder.Application.Contracts/User/Dto/OrganizationUnitDto.cs
+++ src/AeFinder.Application.Contracts/User/Dto/OrganizationUnitDto.cs
@@ -4,7 +4,7 @@ namespace AeFinder.User.Dto;
 
 public class OrganizationUnitDto
 {
-    public string Id { get; set; }
+    public Guid Id { get; set; }
     public string DisplayName { get; set; }
     public DateTime CreationTime { get; set; }
 }
