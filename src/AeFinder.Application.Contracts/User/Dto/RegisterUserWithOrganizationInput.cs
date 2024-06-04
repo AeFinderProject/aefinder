982f40dd422666023ab8f49c14932a4f2ffe29c8
AeFinderBourne <astro@aefinder.io>
2024-06-04 15:06:56 +0800

    feat: limit organization username passowrd regular expression & length

--- src/AeFinder.Application.Contracts/User/Dto/RegisterUserWithOrganizationInput.cs
+++ src/AeFinder.Application.Contracts/User/Dto/RegisterUserWithOrganizationInput.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AeFinder.User.Dto;
 
 public class RegisterUserWithOrganizationInput
 {
+    [Required]
+    [MaxLength(50, ErrorMessage = "UserName cannot exceed 50 characters.")]
+    [RegularExpression(@"^[a-zA-Z0-9\-_]*$", ErrorMessage = "UserName can only contain letters, digits, hyphens, and underscores.")]
     public string UserName { get; set; }
     
+    [Required]
+    [StringLength(50, MinimumLength = 8, ErrorMessage = "The password must be at least 8 characters long and not exceed 50 characters.")]
     public string Password { get; set; }
     
     public string Email { get; set; }
