4c521a6db2a0172cb10ff95eb9a0a89e5e86a1e8
jacob <jacob@aefinder.io>
2024-06-18 00:50:22 +0800

    Resolve json deserialize problem

--- src/AeFinder.Kubernetes/ResourceDefinition/ServiceMonitor.cs
+++ src/AeFinder.Kubernetes/ResourceDefinition/ServiceMonitor.cs
@@ -1,36 +1,50 @@
+using System.Text.Json.Serialization;
 using k8s.Models;
 
 namespace AeFinder.Kubernetes.ResourceDefinition;
 
 public class ServiceMonitor
 {
+    [JsonPropertyName("apiVersion")]
     public string ApiVersion { get; set; } = "monitoring.coreos.com/v1";
+    [JsonPropertyName("kind")]
     public string Kind { get; set; } = "ServiceMonitor";
+    [JsonPropertyName("metadata")]
     public V1ObjectMeta Metadata { get; set; }
+    [JsonPropertyName("spec")]
     public ServiceMonitorSpec Spec { get; set; }
 }
 
 public class ServiceMonitorSpec
 {
+    [JsonPropertyName("endpoints")]
     public List<Endpoint> Endpoints { get; set; }
+    [JsonPropertyName("namespaceSelector")]
     public NamespaceSelector NamespaceSelector { get; set; }
+    [JsonPropertyName("jobLabel")]
     public string JobLabel { get; set; }
+    [JsonPropertyName("selector")]
     public Selector Selector { get; set; }
 }
 
 public class Endpoint
 {
+    [JsonPropertyName("port")]
     public string Port { get; set; }
+    [JsonPropertyName("interval")]
     public string Interval { get; set; }
+    [JsonPropertyName("path")]
     public string Path { get; set; }
 }
 
 public class NamespaceSelector
 {
+    [JsonPropertyName("matchNames")]
     public List<string> MatchNames { get; set; }
 }
 
 public class Selector
 {
+    [JsonPropertyName("matchLabels")]
     public Dictionary<string, string> MatchLabels { get; set; }
 }
--- src/AeFinder.Kubernetes/ResourceDefinition/ServiceMonitorList.cs
+++ src/AeFinder.Kubernetes/ResourceDefinition/ServiceMonitorList.cs
@@ -1,7 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace AeFinder.Kubernetes.ResourceDefinition;
 
 public class ServiceMonitorList
 {
+    [JsonPropertyName("apiVersion")]
     public string ApiVersion { get; set; }
+    [JsonPropertyName("items")]
     public List<ServiceMonitor> Items { get; set; }
 }
