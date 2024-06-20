b5d2ca6316584aaf6e2f9c8f678e5463535b5a1a
AeFinderBourne <astro@aefinder.io>
2024-06-20 15:21:14 +0800

    feat: search log by level

--- src/AeFinder.Logger/ILogService.cs
+++ src/AeFinder.Logger/ILogService.cs
@@ -5,10 +5,8 @@ namespace AeFinder.Logger;
 public interface ILogService
 {
     Task<List<AppLogIndex>> GetAppLatestLogAsync(string indexName, int pageSize,
-        int eventId, string appVersion);
-    Task<List<AppLogIndex>> GetAppLogByStartTimeAsync(string indexName, int pageSize, string startTime,
-        int eventId, string appVersion);
+        int eventId, string appVersion, string level, string searchKeyWord);
 
     Task<List<AppLogIndex>> GetAppLogByStartTimeAsync(string indexName, int pageSize, string startTime,
-        int eventId, string appVersion, string logId);
+        int eventId, string appVersion, string level, string logId, string searchKeyWord);
 }
--- src/AeFinder.Logger/LogElasticSearchService.cs
+++ src/AeFinder.Logger/LogElasticSearchService.cs
@@ -11,66 +11,77 @@ public class LogElasticSearchService:ILogService
     {
         _elasticClient = elasticClient;
     }
-    
+
     public async Task<List<AppLogIndex>> GetAppLatestLogAsync(string indexName, int pageSize,
-        int eventId, string appVersion)
+        int eventId, string appVersion, string level, string searchKeyWord)
     {
+        var mustQuery = new List<Func<QueryContainerDescriptor<AppLogIndex>, QueryContainer>>();
+        mustQuery.Add(m => m.Term(t => t.Field(f => f.App_log.EventId).Value(eventId)));
+        mustQuery.Add(m => m.Term(t => t.Field(f => f.App_log.Version).Value(appVersion)));
+        if (!level.IsNullOrEmpty())
+        {
+            mustQuery.Add(q => q.Term(i => i.Field(f => f.App_log.Level).Value(level)));
+        }
+
+        if (!string.IsNullOrEmpty(searchKeyWord))
+        {
+            mustQuery.Add(q => q.Wildcard(w => w
+                .Field(f => f.App_log.Message)
+                .Value($"*{searchKeyWord}*")));
+        }
+
+        QueryContainer Filter(QueryContainerDescriptor<AppLogIndex> f) => f.Bool(b => b.Must(mustQuery));
         var response = await _elasticClient.SearchAsync<AppLogIndex>(s => s
             .Index(indexName)
             .Sort(so => so
                 .Descending(f => f.App_log.Time))
             .Size(pageSize)
-            .Query(q => q
-                .Bool(b => b
-                    .Must(
-                        m => m.Term(t => t.Field(f => f.App_log.EventId).Value(eventId)),
-                        m => m.Term(t => t.Field(f => f.App_log.Version).Value(appVersion))
-                    ))));
+            .Query(Filter));
 
         return response.Documents.ToList();
     }
 
     public async Task<List<AppLogIndex>> GetAppLogByStartTimeAsync(string indexName, int pageSize, string startTime,
-        int eventId, string appVersion)
+        int eventId, string appVersion, string level, string logId, string searchKeyWord)
     {
+        var mustQuery = new List<Func<QueryContainerDescriptor<AppLogIndex>, QueryContainer>>();
+        mustQuery.Add(m => m.Term(t => t.Field(f => f.App_log.EventId).Value(eventId)));
+        mustQuery.Add(m => m.Term(t => t.Field(f => f.App_log.Version).Value(appVersion)));
+        if (!level.IsNullOrEmpty())
+        {
+            mustQuery.Add(q => q.Term(i => i.Field(f => f.App_log.Level).Value(level)));
+        }
+
+        QueryContainer Filter(QueryContainerDescriptor<AppLogIndex> f) => f.Bool(b => b.Must(mustQuery));
+
+        Func<SortDescriptor<AppLogIndex>, IPromise<IList<ISort>>> sort = null;
         var searchAfter = new List<string>();
         searchAfter.Add(startTime);
+        if (logId.IsNullOrEmpty())
+        {
+            sort = s =>
+                s.Ascending(a => a.App_log.Time);
+        }
+        else
+        {
+            searchAfter.Add(logId);
+            sort = s =>
+                s.Ascending(a => a.App_log.Time).Ascending(d => d.Log_id);
+        }
+
+        if (!string.IsNullOrEmpty(searchKeyWord))
+        {
+            mustQuery.Add(q => q.Wildcard(w => w
+                .Field(f => f.App_log.Message)
+                .Value($"*{searchKeyWord}*")));
+        }
 
         var response = await _elasticClient.SearchAsync<AppLogIndex>(s => s
             .Index(indexName)
-            .Sort(so => so
-                .Ascending(f => f.App_log.Time))
+            .Sort(sort)
             .SearchAfter(searchAfter.Cast<object>().ToArray())
             .Size(pageSize)
-            .Query(q => q
-                .Bool(b => b
-                    .Must(
-                        m => m.Term(t => t.Field(f => f.App_log.EventId).Value(eventId)),
-                        m => m.Term(t => t.Field(f => f.App_log.Version).Value(appVersion))
-                    ))));
-
-        return response.Documents.ToList();
-    }
-
-    public async Task<List<AppLogIndex>> GetAppLogByStartTimeAsync(string indexName, int pageSize, string startTime,
-        int eventId, string appVersion, string logId)
-    {
-        var searchAfter = new List<string>();
-        searchAfter.Add(startTime);
-        searchAfter.Add(logId);
-        var response = await _elasticClient.SearchAsync<AppLogIndex>(s => s
-            .Index(indexName)
-            .Sort(so => so
-                .Ascending(f => f.App_log.Time)
-                .Ascending(f => f.Log_id))
-            .SearchAfter(searchAfter.Cast<object>().ToArray())
-            .Size(pageSize)
-            .Query(q => q
-                .Bool(b => b
-                    .Must(
-                        m => m.Term(t => t.Field(f => f.App_log.EventId).Value(eventId)),
-                        m => m.Term(t => t.Field(f => f.App_log.Version).Value(appVersion))
-                    ))));
+            .Query(Filter));
 
         return response.Documents.ToList();
     }
