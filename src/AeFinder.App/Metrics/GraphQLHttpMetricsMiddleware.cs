d4868263b67377f4bd3d9aeb4a0c00a764d66c2f
jacob <jacob@aefinder.io>
2024-07-02 11:06:20 +0800

    feat: add date tag for aefinder app GraphQL request metrics

--- src/AeFinder.App/Metrics/GraphQLHttpMetricsMiddleware.cs
+++ src/AeFinder.App/Metrics/GraphQLHttpMetricsMiddleware.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics.Metrics;
 using Microsoft.AspNetCore.Http;
+using Volo.Abp.Timing;
 
 namespace AeFinder.App.Metrics;
 
@@ -7,16 +8,19 @@ public class GraphQLHttpMetricsMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly Counter<long> _graphQLRequestsCounter;
+    private readonly IClock _clock;
 
-    public GraphQLHttpMetricsMiddleware(RequestDelegate next, Instrumentation instrumentation)
+    public GraphQLHttpMetricsMiddleware(RequestDelegate next, Instrumentation instrumentation, IClock clock)
     {
         _next = next;
         _graphQLRequestsCounter = instrumentation.GraphQLRequestsCounter;
+        _clock = clock;
     }
 
     public async Task InvokeAsync(HttpContext context)
     {
-        _graphQLRequestsCounter.Add(1,_graphQLRequestsCounter!.Tags!.First());
+        _graphQLRequestsCounter.Add(1, _graphQLRequestsCounter!.Tags!.First(),
+            new KeyValuePair<string, object>("date", _clock.Now.Date.ToString("yyyy-MM-dd")));
         // Call the next delegate/middleware in the pipeline.
         await _next(context);
     }
