9f3c9c64c8224f3d2cb053b4ac8ec58f5dc443ff
AeFinderBourne <astro@aefinder.io>
2024-07-09 23:40:57 +0800

    fix: remove serializer of AppBlockStateChangeState

--- src/AeFinder.Grains/State/BlockStates/AppBlockStateChangeState.cs
+++ src/AeFinder.Grains/State/BlockStates/AppBlockStateChangeState.cs
@@ -1,15 +1,14 @@
 namespace AeFinder.Grains.State.BlockStates;
 
-[GenerateSerializer]
+
 public class AppBlockStateChangeState
 {
-    [Id(0)] public long BlockHeight { get; set; }
-    [Id(1)] public Dictionary<string, BlockStateChange> Changes { get; set; }
+    public long BlockHeight { get; set; }
+    public Dictionary<string, BlockStateChange> Changes { get; set; }
 }
 
-[GenerateSerializer]
 public class BlockStateChange
 {
-    [Id(0)] public string Key { get; set; }
-    [Id(1)] public string Type { get; set; }
+    public string Key { get; set; }
+    public string Type { get; set; }
 }
