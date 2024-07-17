b64ca416e234d1b6bc8628109426573e754d3536
AeFinderBourne <astro@aefinder.io>
2024-07-17 18:30:13 +0800

    feat: logger dictionary by chain id

--- src/AeFinder.App/Logging/AeFinderLogger.cs
+++ src/AeFinder.App/Logging/AeFinderLogger.cs
@@ -1,31 +1,59 @@
+using System.Collections.Concurrent;
 using AeFinder.App.BlockProcessing;
 using AeFinder.App.OperationLimits;
 using AeFinder.Sdk.Logging;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Serilog;
+using Serilog.Events;
 using Volo.Abp.DependencyInjection;
 
 namespace AeFinder.App.Logging;
 
 public class AeFinderLogger : IAeFinderLogger, ISingletonDependency
 {
-    private readonly ILogger<AeFinderLogger> _logger;
+    // private readonly ILogger<AeFinderLogger> _logger;
     private readonly ILogOperationLimitProvider _logOperationLimitProvider;
     private readonly IBlockProcessingContext _blockProcessingContext;
+    private readonly ConcurrentDictionary<string, ILogger<AeFinderLogger>> _loggers;
+    private readonly AppInfoOptions _appInfoOptions;
+    private readonly IConfiguration _configuration;
 
-    public AeFinderLogger(ILogger<AeFinderLogger> logger, ILogOperationLimitProvider logOperationLimitProvider,IBlockProcessingContext blockProcessingContext)
+    public AeFinderLogger(ConcurrentDictionary<string, ILogger<AeFinderLogger>> loggers, IConfiguration configuration,
+        IOptionsSnapshot<AppInfoOptions> appInfoOptions, ILogOperationLimitProvider logOperationLimitProvider,
+        IBlockProcessingContext blockProcessingContext)
     {
-        _logger = logger;
+        // _logger = logger;
+        _appInfoOptions = appInfoOptions.Value;
         _logOperationLimitProvider = logOperationLimitProvider;
         _blockProcessingContext = blockProcessingContext;
+        _loggers = loggers;
+        _configuration = configuration;
     }
 
     public void Log(LogLevel logLevel, Exception exception, string message, params object[] args)
     {
+        // _logOperationLimitProvider.CheckLog(exception, message, args);
+        // List<object> argList = new List<object>();
+        // argList.Add(_blockProcessingContext.ChainId);
+        // argList.AddRange(args.ToList());
+        // _logger.Log(logLevel, AeFinderApplicationConsts.AppLogEventId, exception, "ChainId:{ChainId} " + message,
+        //     argList.ToArray());
+
+        if (!_loggers.TryGetValue(_blockProcessingContext.ChainId, out var logger))
+        {
+            logger = LoggerFactory.Create(c => new LoggerConfiguration()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                    .Enrich.FromLogContext()
+                    .Enrich.WithProperty("AppId", _appInfoOptions.AppId)
+                    .Enrich.WithProperty("Version", _appInfoOptions.Version)
+                    .Enrich.WithProperty("ChainId", _blockProcessingContext.ChainId)
+                    .ReadFrom.Configuration(_configuration))
+                .CreateLogger<AeFinderLogger>();
+        }
+
         _logOperationLimitProvider.CheckLog(exception, message, args);
-        List<object> argList = new List<object>();
-        argList.Add(_blockProcessingContext.ChainId);
-        argList.AddRange(args.ToList());
-        _logger.Log(logLevel, AeFinderApplicationConsts.AppLogEventId, exception, "ChainId:{ChainId} " + message,
-            argList.ToArray());
+        logger.Log(logLevel,AeFinderApplicationConsts.AppLogEventId, exception, message, args);
     }
 }
