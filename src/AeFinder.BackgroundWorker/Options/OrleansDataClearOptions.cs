d68ccd1e16352db659a52280afb5e30f51399599
AeFinderBourne <astro@aefinder.io>
2024-08-06 10:47:35 +0800

    feat: optimize period clear limit count

--- src/AeFinder.BackgroundWorker/Options/OrleansDataClearOptions.cs
+++ src/AeFinder.BackgroundWorker/Options/OrleansDataClearOptions.cs
@@ -7,5 +7,5 @@ public class OrleansDataClearOptions
     public string AppBlockStateChangeGrainIdPrefix { get; set; }
     public string AppStateGrainIdPrefix { get; set; }
     public int ClearTaskPeriodMilliSeconds { get; set; } = 180000;
-    public int PeriodClearLimitCount { get; set; } = 1000;
+    public int PeriodClearLimitCount { get; set; } = 100000;
 }
