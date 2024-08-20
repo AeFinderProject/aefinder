69513e32fde1a1eca21cec19b7a3c4a8deac9da0
AeFinderBourne <astro@aefinder.io>
2024-08-20 18:42:34 +0800

    feat: add amazon client service

---
+++ src/AeFinder.Application.Contracts/AmazonCloud/IAwsS3ClientService.cs
@@ -0,0 +1,6 @@
+namespace AeFinder.AmazonCloud;
+
+public interface IAwsS3ClientService
+{
+    
+}
---
+++ src/AeFinder.Application/AmazonCloud/AmazonS3Options.cs
@@ -0,0 +1,9 @@
+namespace AeFinder.AmazonCloud;
+
+public class AmazonS3Options
+{
+    public string AccessKeyID { get; set; }
+    public string SecretKey { get; set; }
+    public string BucketName { get; set; }
+    public string ServiceURL { get; set; }
+}
