caeb4d9d5bd9a9f5b19a470bd1ab7e56dc095eda
AeFinderBourne <astro@aefinder.io>
2024-08-22 21:06:44 +0800

    feat: optimize parameter

--- src/AeFinder.HttpApi/Models/UpdateAttachmentInput.cs
+++ src/AeFinder.HttpApi/Models/UpdateAttachmentInput.cs
@@ -8,10 +8,11 @@ namespace AeFinder.Models;
 
 public class UpdateAttachmentInput
 {
+    [ModelBinder(BinderType = typeof(JsonModelBinder))]
+    public string AttachmentDeleteFileKeyList { get; set; }
     public IFormFile Attachment1 { get; set; }
     public IFormFile Attachment2 { get; set; }
     public IFormFile Attachment3 { get; set; }
     public IFormFile Attachment4 { get; set; }
     public IFormFile Attachment5 { get; set; }
-    public string AttachmentDeleteFileKeyList { get; set; }
 }
