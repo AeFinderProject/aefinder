3b6e2c01656f44263fe291278faa52943ab1251d
jacob <jacob@aefinder.io>
2024-08-24 10:17:58 +0800

    chore: remove commented-out code

--- src/AeFinder.HttpApi/Models/AddSubscriptionInput.cs
+++ src/AeFinder.HttpApi/Models/AddSubscriptionInput.cs
@@ -12,9 +12,4 @@ public class AddSubscriptionInput
     public SubscriptionManifestDto Manifest { get; set; }
     public IFormFile Code { get; set; }
     public List<IFormFile> AttachmentList { get; set; }
-    // public IFormFile Attachment1 { get; set; }
-    // public IFormFile Attachment2 { get; set; }
-    // public IFormFile Attachment3 { get; set; }
-    // public IFormFile Attachment4 { get; set; }
-    // public IFormFile Attachment5 { get; set; }
 }
--- src/AeFinder.HttpApi/Models/UpdateSubscriptionCodeInput.cs
+++ src/AeFinder.HttpApi/Models/UpdateSubscriptionCodeInput.cs
@@ -8,9 +8,4 @@ public class UpdateSubscriptionCodeInput
     public IFormFile Code { get; set; }
     public string AttachmentDeleteFileKeyList { get; set; }
     public List<IFormFile> AttachmentList { get; set; }
-    // public IFormFile Attachment1 { get; set; }
-    // public IFormFile Attachment2 { get; set; }
-    // public IFormFile Attachment3 { get; set; }
-    // public IFormFile Attachment4 { get; set; }
-    // public IFormFile Attachment5 { get; set; }
 }
