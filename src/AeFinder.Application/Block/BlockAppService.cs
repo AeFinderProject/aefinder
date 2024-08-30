90b1eb6efd11d734f6d1f23791a8b35399d62e69
sinclair <sinclair@aefinder.io>
2024-08-30 20:41:40 +0800

    feat: use search after

--- src/AeFinder.Application/Block/BlockAppService.cs
+++ src/AeFinder.Application/Block/BlockAppService.cs
@@ -15,17 +15,17 @@ namespace AeFinder.Block;
 
 [RemoteService(IsEnabled = false)]
 [DisableAuditing]
-public class BlockAppService:ApplicationService,IBlockAppService
+public class BlockAppService : ApplicationService, IBlockAppService
 {
     private readonly IEntityMappingRepository<BlockIndex, string> _blockIndexRepository;
     private readonly IEntityMappingRepository<TransactionIndex, string> _transactionIndexRepository;
     private readonly IEntityMappingRepository<LogEventIndex, string> _logEventIndexRepository;
     private readonly ApiOptions _apiOptions;
     private readonly IEntityMappingRepository<SummaryIndex, string> _summaryIndexRepository;
-    
+
     public BlockAppService(IEntityMappingRepository<BlockIndex, string> blockIndexRepository,
-        IEntityMappingRepository<TransactionIndex,string> transactionIndexRepository,
-        IEntityMappingRepository<LogEventIndex,string> logEventIndexRepository,
+        IEntityMappingRepository<TransactionIndex, string> transactionIndexRepository,
+        IEntityMappingRepository<LogEventIndex, string> logEventIndexRepository,
         IOptionsSnapshot<ApiOptions> apiOptions,
         IEntityMappingRepository<SummaryIndex, string> summaryIndexRepository)
     {
@@ -42,6 +42,7 @@ public class BlockAppService:ApplicationService,IBlockAppService
         {
             input.EndBlockHeight = input.StartBlockHeight + _apiOptions.BlockQueryHeightInterval - 1;
         }
+
         List<BlockDto> items = new List<BlockDto>();
         Expression<Func<BlockIndex, bool>> expression = p =>
             p.ChainId == input.ChainId && p.BlockHeight >= input.StartBlockHeight &&
@@ -51,26 +52,33 @@ public class BlockAppService:ApplicationService,IBlockAppService
             expression = p =>
                 p.ChainId == input.ChainId && p.BlockHash == input.BlockHash;
         }
+
         if (input.IsOnlyConfirmed)
         {
             expression = expression.And(p => p.Confirmed == input.IsOnlyConfirmed);
         }
+
         var queryable = await _blockIndexRepository.GetQueryableAsync();
-        var list = queryable.Where(expression).OrderBy(p => p.BlockHeight).Skip(0).Take(10000).ToList();
-        if (list.Count == 10000)
+        queryable = queryable.Where(expression).OrderBy(o => o.BlockHeight).OrderBy(o => o.Id)
+            .Take(_apiOptions.MaxQuerySize);
+        var list = queryable.ToList();
+        while (list.Count % _apiOptions.MaxQuerySize == 0)
         {
-            list = queryable.Where(expression).OrderBy(p => p.BlockHeight).Skip(0).Take(20000).ToList();
-            if (list.Count == 20000)
+            var blocks = queryable.After(new object[] { list.Last().BlockHeight, list.Last().Id }).ToList();
+            if (blocks.Count == 0)
             {
-                list = queryable.Where(expression).OrderBy(p => p.BlockHeight).Skip(0).Take(int.MaxValue).ToList();
+                break;
             }
+
+            list.AddRange(blocks);
         }
+
         items = ObjectMapper.Map<List<BlockIndex>, List<BlockDto>>(list);
         List<BlockDto> resultList = new List<BlockDto>();
         resultList.AddRange(items);
         return resultList;
     }
-    
+
     public async Task<long> GetBlockCountAsync(GetBlocksInput input)
     {
         Expression<Func<BlockIndex, bool>> expression = p =>
@@ -81,44 +89,50 @@ public class BlockAppService:ApplicationService,IBlockAppService
             expression = p =>
                 p.ChainId == input.ChainId && p.BlockHash == input.BlockHash;
         }
+
         if (input.IsOnlyConfirmed)
         {
             expression = expression.And(p => p.Confirmed == input.IsOnlyConfirmed);
         }
-    
+
         var count = await _blockIndexRepository.GetCountAsync(expression);
-        
+
         return count;
     }
-    
-   public async Task<List<TransactionDto>> GetTransactionsAsync(GetTransactionsInput input)
+
+    public async Task<List<TransactionDto>> GetTransactionsAsync(GetTransactionsInput input)
     {
         if ((input.EndBlockHeight - input.StartBlockHeight + 1) > _apiOptions.TransactionQueryHeightInterval)
         {
             input.EndBlockHeight = input.StartBlockHeight + _apiOptions.TransactionQueryHeightInterval - 1;
         }
+
         var queryable = await _transactionIndexRepository.GetQueryableAsync();
 
         List<TransactionDto> resultList = new List<TransactionDto>();
-        if (input.Events != null && input.Events.Count>0)
+        if (input.Events != null && input.Events.Count > 0)
         {
-            Expression<Func<TransactionIndex, bool>> mustQuery = p=>p.LogEvents.Any(i=>i.ChainId == input.ChainId && i.BlockHeight >= input.StartBlockHeight && i.BlockHeight <= input.EndBlockHeight);
-            if(!string.IsNullOrEmpty(input.TransactionId))
+            Expression<Func<TransactionIndex, bool>> mustQuery = p => p.LogEvents.Any(i =>
+                i.ChainId == input.ChainId && i.BlockHeight >= input.StartBlockHeight &&
+                i.BlockHeight <= input.EndBlockHeight);
+            if (!string.IsNullOrEmpty(input.TransactionId))
             {
-                mustQuery = p=>p.LogEvents.Any(i=>i.ChainId == input.ChainId && i.TransactionId == input.TransactionId);
+                mustQuery = p =>
+                    p.LogEvents.Any(i => i.ChainId == input.ChainId && i.TransactionId == input.TransactionId);
             }
+
             if (input.IsOnlyConfirmed)
             {
-                mustQuery = mustQuery.And(p=>p.LogEvents.Any(i=>i.Confirmed == input.IsOnlyConfirmed));
+                mustQuery = mustQuery.And(p => p.LogEvents.Any(i => i.Confirmed == input.IsOnlyConfirmed));
             }
-            
+
             Expression<Func<TransactionIndex, bool>> shouldQuery = null;
             foreach (var eventInput in input.Events)
             {
                 Expression<Func<TransactionIndex, bool>> shouldMustQuery = null;
                 if (!string.IsNullOrEmpty(eventInput.ContractAddress))
                 {
-                    shouldMustQuery = p=>p.LogEvents.Any(i=>i.ContractAddress == eventInput.ContractAddress);
+                    shouldMustQuery = p => p.LogEvents.Any(i => i.ContractAddress == eventInput.ContractAddress);
                 }
 
                 Expression<Func<TransactionIndex, bool>> ShouldMustShouldQuery = null;
@@ -137,61 +151,78 @@ public class BlockAppService:ApplicationService,IBlockAppService
 
                 if (ShouldMustShouldQuery != null)
                 {
-                    shouldMustQuery = shouldMustQuery is null ? ShouldMustShouldQuery : shouldMustQuery.And(ShouldMustShouldQuery);
+                    shouldMustQuery = shouldMustQuery is null
+                        ? ShouldMustShouldQuery
+                        : shouldMustQuery.And(ShouldMustShouldQuery);
                 }
 
                 shouldQuery = shouldQuery is null ? shouldMustQuery : shouldQuery.Or(shouldMustQuery);
-               
+
             }
+
             mustQuery = mustQuery.And(shouldQuery);
-            var list = queryable.Where(mustQuery).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(10000).ToList();
-            if (list.Count == 10000)
+
+            var query = queryable.Where(mustQuery).OrderBy(o => o.BlockHeight).OrderBy(o => o.Index).OrderBy(o => o.Id)
+                .Take(_apiOptions.MaxQuerySize);
+            var list = query.ToList();
+            while (list.Count % _apiOptions.MaxQuerySize == 0)
             {
-                list = queryable.Where(mustQuery).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(20000).ToList();
-                if (list.Count == 20000)
+                var transactions = query
+                    .After(new object[] { list.Last().BlockHeight, list.Last().Index, list.Last().Id }).ToList();
+                if (transactions.Count == 0)
                 {
-                    list = queryable.Where(mustQuery).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(int.MaxValue).ToList();
+                    break;
                 }
+
+                list.AddRange(transactions);
             }
+
             resultList = ObjectMapper.Map<List<TransactionIndex>, List<TransactionDto>>(list);
         }
         else
         {
-            Expression<Func<TransactionIndex, bool>> mustQuery = p => p.ChainId == input.ChainId && p.BlockHeight >= input.StartBlockHeight && p.BlockHeight <= input.EndBlockHeight;
+            Expression<Func<TransactionIndex, bool>> mustQuery = p =>
+                p.ChainId == input.ChainId && p.BlockHeight >= input.StartBlockHeight &&
+                p.BlockHeight <= input.EndBlockHeight;
             if (!string.IsNullOrEmpty(input.TransactionId))
             {
                 mustQuery = p => p.ChainId == input.ChainId && p.TransactionId == input.TransactionId;
             }
+
             if (input.IsOnlyConfirmed)
             {
                 mustQuery = mustQuery.And(p => p.Confirmed == input.IsOnlyConfirmed);
             }
-            
-            var list = queryable.Where(mustQuery).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(10000).ToList();
 
-            if (list.Count == 10000)
+            var query = queryable.Where(mustQuery).OrderBy(o => o.BlockHeight).OrderBy(o => o.Index).OrderBy(o => o.Id)
+                .Take(_apiOptions.MaxQuerySize);
+            var list = query.ToList();
+            while (list.Count % _apiOptions.MaxQuerySize == 0)
             {
-                list = queryable.Where(mustQuery).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(20000).ToList();
-
-                if (list.Count == 20000)
+                var transactions = query
+                    .After(new object[] { list.Last().BlockHeight, list.Last().Index, list.Last().Id }).ToList();
+                if (transactions.Count == 0)
                 {
-                    list = queryable.Where(mustQuery).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(int.MaxValue).ToList();
+                    break;
                 }
+
+                list.AddRange(transactions);
             }
+
             resultList = ObjectMapper.Map<List<TransactionIndex>, List<TransactionDto>>(list);
         }
-        
+
 
         return resultList;
     }
-    
-     public async Task<List<LogEventDto>> GetLogEventsAsync(GetLogEventsInput input)
+
+    public async Task<List<LogEventDto>> GetLogEventsAsync(GetLogEventsInput input)
     {
         if ((input.EndBlockHeight - input.StartBlockHeight + 1) > _apiOptions.LogEventQueryHeightInterval)
         {
             input.EndBlockHeight = input.StartBlockHeight + _apiOptions.LogEventQueryHeightInterval - 1;
         }
-        
+
         Expression<Func<LogEventIndex, bool>> mustQuery = p =>
             p.ChainId == input.ChainId && p.BlockHeight >= input.StartBlockHeight &&
             p.BlockHeight <= input.EndBlockHeight;
@@ -200,8 +231,8 @@ public class BlockAppService:ApplicationService,IBlockAppService
         {
             mustQuery = mustQuery.And(p => p.Confirmed == input.IsOnlyConfirmed);
         }
-        
-        if (input.Events != null && input.Events.Count>0)
+
+        if (input.Events != null && input.Events.Count > 0)
         {
             Expression<Func<LogEventIndex, bool>> shouldQuery = null;
             foreach (var eventInput in input.Events)
@@ -219,14 +250,18 @@ public class BlockAppService:ApplicationService,IBlockAppService
                     {
                         if (!string.IsNullOrEmpty(eventName))
                         {
-                            shouldMushShouldQuery = shouldMushShouldQuery is null ? p => p.EventName == eventName : shouldMushShouldQuery.Or(p => p.EventName == eventName);
+                            shouldMushShouldQuery = shouldMushShouldQuery is null
+                                ? p => p.EventName == eventName
+                                : shouldMushShouldQuery.Or(p => p.EventName == eventName);
                         }
                     }
                 }
 
                 if (shouldMushShouldQuery != null)
                 {
-                    shouldMustQuery = shouldMustQuery is null ? shouldMushShouldQuery : shouldMustQuery.And(shouldMushShouldQuery);
+                    shouldMustQuery = shouldMustQuery is null
+                        ? shouldMushShouldQuery
+                        : shouldMustQuery.And(shouldMushShouldQuery);
                 }
 
                 shouldQuery = shouldQuery is null ? shouldMustQuery : shouldQuery.Or(shouldMustQuery);
@@ -234,40 +269,46 @@ public class BlockAppService:ApplicationService,IBlockAppService
 
             mustQuery = mustQuery.And(shouldQuery);
         }
-        
-        List<LogEventDto> resultList = new List<LogEventDto>();
+
         var queryable = await _logEventIndexRepository.GetQueryableAsync();
-        var list = queryable.Where(mustQuery).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(10000).ToList();
-        if (list.Count == 10000)
+        queryable = queryable.Where(mustQuery).OrderBy(o => o.BlockHeight).OrderBy(o => o.Index).OrderBy(o => o.Id)
+            .Take(_apiOptions.MaxQuerySize);
+        var list = queryable.ToList();
+        while (list.Count % _apiOptions.MaxQuerySize == 0)
         {
-            list = queryable.Where(mustQuery).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(20000).ToList();
-            if (list.Count == 20000)
+            var logEvents = queryable
+                .After(new object[] { list.Last().BlockHeight, list.Last().Index, list.Last().Id }).ToList();
+            if (logEvents.Count == 0)
             {
-                list = queryable.Where(mustQuery).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(int.MaxValue).ToList();
+                break;
             }
+
+            list.AddRange(logEvents);
         }
-        
-        resultList = ObjectMapper.Map<List<LogEventIndex>, List<LogEventDto>>(list);
-        
+
+        var resultList = ObjectMapper.Map<List<LogEventIndex>, List<LogEventDto>>(list);
         return resultList;
     }
 
-     public async Task<List<TransactionDto>> GetSubscriptionTransactionsAsync(GetSubscriptionTransactionsInput input)
-     {
-         if ((input.EndBlockHeight - input.StartBlockHeight + 1) > _apiOptions.TransactionQueryHeightInterval)
+    public async Task<List<TransactionDto>> GetSubscriptionTransactionsAsync(GetSubscriptionTransactionsInput input)
+    {
+        if ((input.EndBlockHeight - input.StartBlockHeight + 1) > _apiOptions.TransactionQueryHeightInterval)
         {
             input.EndBlockHeight = input.StartBlockHeight + _apiOptions.TransactionQueryHeightInterval - 1;
         }
+
         var queryable = await _transactionIndexRepository.GetQueryableAsync();
 
         var resultList = new List<TransactionDto>();
-        Expression<Func<TransactionIndex, bool>> query = q => q.ChainId == input.ChainId && q.BlockHeight >= input.StartBlockHeight && q.BlockHeight <= input.EndBlockHeight;
-        
+        Expression<Func<TransactionIndex, bool>> query = q =>
+            q.ChainId == input.ChainId && q.BlockHeight >= input.StartBlockHeight &&
+            q.BlockHeight <= input.EndBlockHeight;
+
         if (input.IsOnlyConfirmed)
         {
             query = query.And(p => p.Confirmed == input.IsOnlyConfirmed);
         }
-        
+
         Expression<Func<TransactionIndex, bool>> transactionQuery = null;
         if (input.TransactionFilters != null && input.TransactionFilters.Count > 0)
         {
@@ -277,7 +318,7 @@ public class BlockAppService:ApplicationService,IBlockAppService
                 if (!string.IsNullOrEmpty(transactionInput.To))
                 {
                     shouldMustQuery = q => q.To == transactionInput.To;
-                    
+
                     Expression<Func<TransactionIndex, bool>> ShouldMustShouldQuery = null;
                     foreach (var methodName in transactionInput.MethodNames)
                     {
@@ -288,19 +329,21 @@ public class BlockAppService:ApplicationService,IBlockAppService
                                 : ShouldMustShouldQuery.Or(p => p.MethodName == methodName);
                         }
                     }
-                    
+
                     if (ShouldMustShouldQuery != null)
                     {
                         shouldMustQuery = shouldMustQuery.And(ShouldMustShouldQuery);
                     }
 
-                    transactionQuery = transactionQuery is null ? shouldMustQuery : transactionQuery.Or(shouldMustQuery);
+                    transactionQuery = transactionQuery is null
+                        ? shouldMustQuery
+                        : transactionQuery.Or(shouldMustQuery);
                 }
             }
         }
 
         Expression<Func<TransactionIndex, bool>> logEventQuery = null;
-        if (input.LogEventFilters != null && input.LogEventFilters.Count>0)
+        if (input.LogEventFilters != null && input.LogEventFilters.Count > 0)
         {
             foreach (var eventInput in input.LogEventFilters)
             {
@@ -340,29 +383,33 @@ public class BlockAppService:ApplicationService,IBlockAppService
             query = query.And(transactionQuery ?? logEventQuery);
         }
 
-        var list = queryable.Where(query).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(10000).ToList();
+        var list = queryable.Where(query).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(10000)
+            .ToList();
         if (list.Count == 10000)
         {
-            list = queryable.Where(query).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(20000).ToList();
+            list = queryable.Where(query).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(20000)
+                .ToList();
             if (list.Count == 20000)
             {
-                list = queryable.Where(query).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0).Take(int.MaxValue).ToList();
+                list = queryable.Where(query).OrderBy(p => p.BlockHeight).OrderBy(p => p.Index).Skip(0)
+                    .Take(int.MaxValue).ToList();
             }
         }
+
         resultList = ObjectMapper.Map<List<TransactionIndex>, List<TransactionDto>>(list);
-        
+
 
         return resultList;
-     }
+    }
 
-     public async Task<List<SummaryDto>> GetSummariesAsync(GetSummariesInput input)
-     {
-         var resultList = new List<SummaryDto>();
-         Expression<Func<SummaryIndex, bool>> expression = p =>
-             p.ChainId == input.ChainId;
-         var queryable = await _summaryIndexRepository.GetQueryableAsync();
-         var list = queryable.Where(expression).Skip(0).Take(100).ToList();
-         resultList = ObjectMapper.Map<List<SummaryIndex>, List<SummaryDto>>(list);
-         return resultList;
-     }
+    public async Task<List<SummaryDto>> GetSummariesAsync(GetSummariesInput input)
+    {
+        var resultList = new List<SummaryDto>();
+        Expression<Func<SummaryIndex, bool>> expression = p =>
+            p.ChainId == input.ChainId;
+        var queryable = await _summaryIndexRepository.GetQueryableAsync();
+        var list = queryable.Where(expression).Skip(0).Take(100).ToList();
+        resultList = ObjectMapper.Map<List<SummaryIndex>, List<SummaryDto>>(list);
+        return resultList;
+    }
 }
