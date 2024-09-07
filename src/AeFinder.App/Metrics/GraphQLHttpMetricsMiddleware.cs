13501d2fd33c54bcbb094d39db741e6216fb29db
jacob <jacob@aefinder.io>
2024-09-07 21:58:44 +0800

    feat: add timing metrics for AeFinder App GraphQL request

--- src/AeFinder.App/Metrics/GraphQLHttpMetricsMiddleware.cs
+++ src/AeFinder.App/Metrics/GraphQLHttpMetricsMiddleware.cs
@@ -1,32 +1,38 @@
+using System.Diagnostics;
 using System.Diagnostics.Metrics;
-using AElf.OpenTelemetry.ExecutionTime;
+using AeFinder.Metrics;
 using Microsoft.AspNetCore.Http;
 using Volo.Abp.Timing;
 
 namespace AeFinder.App.Metrics;
 
-[AggregateExecutionTime]
 public class GraphQLHttpMetricsMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly Counter<long> _graphQLRequestsCounter;
     private readonly IClock _clock;
     private readonly IAppInfoProvider _appInfoProvider;
+    private readonly IElapsedTimeRecorder _elapsedTimeRecorder;
 
-    public GraphQLHttpMetricsMiddleware(RequestDelegate next, Instrumentation instrumentation, IClock clock, IAppInfoProvider appInfoProvider)
+    public GraphQLHttpMetricsMiddleware(RequestDelegate next, Instrumentation instrumentation, IClock clock,
+        IAppInfoProvider appInfoProvider, IElapsedTimeRecorder elapsedTimeRecorder)
     {
         _next = next;
         _graphQLRequestsCounter = instrumentation.GraphQLRequestsCounter;
         _clock = clock;
         _appInfoProvider = appInfoProvider;
+        _elapsedTimeRecorder = elapsedTimeRecorder;
     }
 
-    public virtual async Task InvokeAsync(HttpContext context)
+    public async Task InvokeAsync(HttpContext context)
     {
         _graphQLRequestsCounter.Add(1, _graphQLRequestsCounter!.Tags!.First(),
             new KeyValuePair<string, object>("date", _clock.Now.Date.ToString("yyyy-MM-dd")),
             new KeyValuePair<string, object>("monitor", _appInfoProvider.AppId));
+        var stopwatch = Stopwatch.StartNew();
         // Call the next delegate/middleware in the pipeline.
         await _next(context);
+        stopwatch.Stop();
+        _elapsedTimeRecorder.Record("AeFinderAppHostGraphQLRequests",stopwatch.ElapsedMilliseconds);
     }
 }
