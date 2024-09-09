17fdb089ad8cb8817324e3053e61634f7d2a5cdc
AeFinderBourne <astro@aefinder.io>
2024-09-09 19:23:04 +0800

    feat: add background worker test module

---
+++ test/AeFinder.BackgroundWorker.Tests/AeFinderBackgroundWorkerTestModule.cs
@@ -0,0 +1,28 @@
+using AeFinder.Orleans.TestBase;
+using Volo.Abp;
+using Volo.Abp.Modularity;
+
+namespace AeFinder.BackgroundWorker.Tests;
+
+[DependsOn(
+    typeof(AeFinderBackGroundModule),
+    typeof(AeFinderDomainTestModule),
+    typeof(AeFinderOrleansTestBaseModule)
+)]
+public class AeFinderBackgroundWorkerTestModule: AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        
+    }
+
+    public override void OnPreApplicationInitialization(ApplicationInitializationContext context)
+    {
+        
+    }
+
+    public override void OnApplicationShutdown(ApplicationShutdownContext context)
+    {
+        
+    }
+}
---
+++ test/AeFinder.BackgroundWorker.Tests/ScheduledTaskTests.cs
@@ -0,0 +1,9 @@
+namespace AeFinder.BackgroundWorker.Tests;
+
+public class ScheduledTaskTests
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
