f3f5aec36fb370c04ca70986e63ac480283f0571
AeFinderBourne <astro@aefinder.io>
2024-10-11 11:21:05 +0800

    perf: remove useless code

--- src/AeFinder.BackgroundWorker.Core/Options/ScheduledTaskOptions.cs
+++ src/AeFinder.BackgroundWorker.Core/Options/ScheduledTaskOptions.cs
@@ -5,5 +5,4 @@ public class ScheduledTaskOptions
     public int AppInfoSyncTaskPeriodMilliSeconds { get; set; } = 86400000;
     public int AppRescanCheckTaskPeriodMilliSeconds { get; set; } = 600000;
     public int MaxAppRescanTimes { get; set; } = 3;
-    public int AppPodInfoSyncTaskPeriodMilliSeconds { get; set; } = 600000;
 }
