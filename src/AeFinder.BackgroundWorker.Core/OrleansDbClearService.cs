c75e5b24bf1c02c3f84e65801c4c1de0c587ae76
AeFinderBourne <astro@aefinder.io>
2024-10-17 15:23:36 +0800

    feat: optimize version data clear

--- src/AeFinder.BackgroundWorker.Core/OrleansDbClearService.cs
+++ src/AeFinder.BackgroundWorker.Core/OrleansDbClearService.cs
@@ -25,13 +25,7 @@ public class OrleansDbClearService : IOrleansDbClearService
     public async Task<List<BsonValue>> QueryRecordIdsWithPrefixAsync(string collectionName, string idPrefix,
         int limitCount)
     {
-        var collection = _database.GetCollection<BsonDocument>(collectionName);
-
-        // Step 1: Query the ids of all records with matching _id prefixes
-        var filter = Builders<BsonDocument>.Filter.Regex("_id", new BsonRegularExpression($"^{idPrefix}"));
-        var projection = Builders<BsonDocument>.Projection.Include("_id");
-        var resultList =
-            await collection.Find(filter).Limit(limitCount).Project<BsonDocument>(projection).ToListAsync();
+        var resultList = await QueryRecordsWithPrefixAsync(collectionName, idPrefix, limitCount);
 
         if (resultList == null || resultList.Count == 0)
         {
@@ -40,10 +34,67 @@ public class OrleansDbClearService : IOrleansDbClearService
             return new List<BsonValue>();
         }
 
+        // do
+        // {
+        //     _logger.LogInformation(
+        //         "The number of query documents needs to be reduced, current count: {limitCount}",
+        //         limitCount);
+        //     limitCount = limitCount / 2;
+        //     resultList = await QueryRecordsWithPrefixAsync(collectionName, idPrefix, limitCount);
+        // } while (!CheckBsonLength(resultList));
+        
+        var totalSize = resultList.Sum(doc =>
+        {
+            var size = doc.ToBson().Length;  // Serialize the document to BSON and get its length.
+            return size;
+        });
+
+        if (totalSize > _orleansDataClearOptions.MongoDbMaxDocumentSize)
+        {
+            _logger.LogInformation(
+                "The total document size exceeds the limit size 16793600, current total size: {totalSize}, current limit count: {limitCount}",
+                totalSize, limitCount);
+            double retentionRatio = (double)_orleansDataClearOptions.MongoDbMaxDocumentSize / totalSize;
+            limitCount = (int)(limitCount * (retentionRatio - _orleansDataClearOptions.ExceedDeviationRatio));
+            resultList = await QueryRecordsWithPrefixAsync(collectionName, idPrefix, limitCount);
+        }
+
         _logger.LogInformation("Found {count} records to delete. collectionName:{collectionName} idPrefix:{idPrefix}",
             resultList.Count, collectionName, idPrefix);
         var ids = resultList.Select(doc => doc["_id"]).ToList();
         return ids;
+    }
+    
+    private async Task<List<BsonDocument>> QueryRecordsWithPrefixAsync(string collectionName, string idPrefix,
+        int limitCount)
+    {
+        var collection = _database.GetCollection<BsonDocument>(collectionName);
+
+        // Step 1: Query the ids of all records with matching _id prefixes
+        var filter = Builders<BsonDocument>.Filter.Regex("_id", new BsonRegularExpression($"^{idPrefix}"));
+        var projection = Builders<BsonDocument>.Projection.Include("_id");
+        var resultList =
+            await collection.Find(filter).Limit(limitCount).Project<BsonDocument>(projection).ToListAsync();
+
+        return resultList;
+    }
+
+    private bool CheckBsonLength(List<BsonDocument> resultList)
+    {
+        var totalSize = resultList.Sum(doc =>
+        {
+            var size = doc.ToBson().Length;  // Serialize the document to BSON and get its length.
+            return size;
+        });
+
+        if (totalSize > 16793600)
+        {
+            _logger.LogInformation("The total document size exceeds the limit, current total size: {totalSize}",
+                totalSize);
+            return false;
+        }
+
+        return true;
     }
     
     public async Task<long> DeleteRecordsWithIdsAsync(string collectionName, List<BsonValue> recordIdList)
