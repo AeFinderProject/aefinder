3da2e5ce04e4a9c1ce926ed6882c96fdc160dd79
AeFinderBourne <156667778+AeFinderBourne@users.noreply.github.com>
2024-10-23 11:17:52 +0800

    Feature:Optimize the automatic deletion of unnecessary version history data in AeIndexer (#217)

--- src/AeFinder.BackgroundWorker.Core/OrleansDbClearService.cs
+++ src/AeFinder.BackgroundWorker.Core/OrleansDbClearService.cs
@@ -25,6 +25,45 @@ public class OrleansDbClearService : IOrleansDbClearService
     public async Task<List<BsonValue>> QueryRecordIdsWithPrefixAsync(string collectionName, string idPrefix,
         int limitCount)
     {
+        var resultList = await QueryRecordsWithPrefixAsync(collectionName, idPrefix, limitCount);
+
+        if (resultList == null || resultList.Count == 0)
+        {
+            _logger.LogInformation("No records found to delete. collectionName:{collectionName} idPrefix:{idPrefix}",
+                collectionName, idPrefix);
+            return new List<BsonValue>();
+        }
+        
+        var totalSize = resultList.Sum(doc =>
+        {
+            var size = doc.ToBson().Length;  // Serialize the document to BSON and get its length.
+            return size;
+        });
+
+        var maxDocumentSize = _orleansDataClearOptions.MongoDbMaxDocumentSize;
+        if (totalSize > maxDocumentSize)
+        {
+            var deviationRatio = _orleansDataClearOptions.ExceedDeviationRatio;
+            _logger.LogInformation(
+                "The total document size exceeds the limit size {maxDocumentSize}, current total size: {totalSize}, current limit count: {limitCount}, current deviation ratio: {deviationRatio}",
+                maxDocumentSize, totalSize, limitCount, deviationRatio);
+            double retentionRatio = (double)maxDocumentSize / totalSize;
+            limitCount = (int)(limitCount * (retentionRatio - deviationRatio));
+            // resultList = await QueryRecordsWithPrefixAsync(collectionName, idPrefix, limitCount);
+            _logger.LogInformation("Finally limit count: {limitCount}, recent radio: {retentionRatio}", limitCount,
+                retentionRatio);
+            resultList = resultList.Take(limitCount).ToList();
+        }
+
+        _logger.LogInformation("Found {count} records to delete. collectionName:{collectionName} idPrefix:{idPrefix}",
+            resultList.Count, collectionName, idPrefix);
+        var ids = resultList.Select(doc => doc["_id"]).ToList();
+        return ids;
+    }
+    
+    private async Task<List<BsonDocument>> QueryRecordsWithPrefixAsync(string collectionName, string idPrefix,
+        int limitCount)
+    {
         var collection = _database.GetCollection<BsonDocument>(collectionName);
 
         // Step 1: Query the ids of all records with matching _id prefixes
@@ -33,17 +72,7 @@ public class OrleansDbClearService : IOrleansDbClearService
         var resultList =
             await collection.Find(filter).Limit(limitCount).Project<BsonDocument>(projection).ToListAsync();
 
-        if (resultList == null || resultList.Count == 0)
-        {
-            _logger.LogInformation("No records found to delete. collectionName:{collectionName} idPrefix:{idPrefix}",
-                collectionName, idPrefix);
-            return new List<BsonValue>();
-        }
-
-        _logger.LogInformation("Found {count} records to delete. collectionName:{collectionName} idPrefix:{idPrefix}",
-            resultList.Count, collectionName, idPrefix);
-        var ids = resultList.Select(doc => doc["_id"]).ToList();
-        return ids;
+        return resultList;
     }
     
     public async Task<long> DeleteRecordsWithIdsAsync(string collectionName, List<BsonValue> recordIdList)
