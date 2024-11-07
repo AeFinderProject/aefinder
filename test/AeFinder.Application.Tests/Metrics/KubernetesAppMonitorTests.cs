38ba2c94fe8c7b1fc0225923b459e9b3a85f3c58
AeFinderBourne <astro@aefinder.io>
2024-11-07 19:22:03 +0800

    feat: kubernetes app monitor unit test

---
+++ test/AeFinder.Application.Tests/Metrics/KubernetesAppMonitorTests.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AeFinder.Apps;
+using AeFinder.Apps.Dto;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace AeFinder.Metrics;
+
+public class KubernetesAppMonitorTests:AeFinderApplicationTestBase
+{
+    private readonly IAppService _appService;
+    private readonly IKubernetesAppMonitor _kubernetesAppMonitor;
+    
+    public KubernetesAppMonitorTests()
+    {
+        _appService = GetRequiredService<IAppService>();
+        _kubernetesAppMonitor = GetRequiredService<IKubernetesAppMonitor>();
+    }
+    
+    protected override void AfterAddApplication(IServiceCollection services)
+    {
+        services.AddSingleton(BuildKubernetesAppMonitor());
+    }
+    
+    private static IKubernetesAppMonitor BuildKubernetesAppMonitor()
+    {
+        var mockKubernetesAppMonitor = new Mock<IKubernetesAppMonitor>();
+        mockKubernetesAppMonitor
+            .Setup(service => service.GetAppPodsResourceInfoFromPrometheusAsync(It.IsAny<List<string>>()))
+            .ReturnsAsync(new List<AppPodResourceInfoDto> { 
+                new AppPodResourceInfoDto
+                {
+                    PodUid="7282799a-b15c-4919-bc78-843410fcfb2e",
+                    PodName="deployment-test-ae8ec5b7361e4e1381082f5570283214-fulmhzcw",
+                    CurrentTime=DateTime.Now,
+                    Containers=new List<PodContainerResourceDto>()
+                    {
+                        new PodContainerResourceDto()
+                        {
+                            ContainerName = "container-ae8ec5b7361e4e1381082f5570283214-full",
+                            CpuUsage = "",
+                            MemoryUsage = ""
+                        },
+                        new PodContainerResourceDto()
+                        {
+                            ContainerName = "filebeat-sidecar",
+                            CpuUsage = "",
+                            MemoryUsage = ""
+                        }
+                    }
+                } 
+            });
+        return mockKubernetesAppMonitor.Object;
+    }
+
+    [Fact]
+    public async Task GetAppPodsResourceInfoFromPrometheus_Test()
+    {
+        var podsName = new List<string>()
+        {
+            "deployment-awaken-e55a9e430bd14ebb95ef81378906fd5f-full-ff7p6g2",
+            "deployment-beangogame-ae8ec5b7361e4e1381082f5570283214-fulmhzcw"
+        };
+        var podResourceInfoDtos=await _kubernetesAppMonitor.GetAppPodsResourceInfoFromPrometheusAsync(podsName);
+        podResourceInfoDtos.Count.ShouldBe(1);
+    }
+}
