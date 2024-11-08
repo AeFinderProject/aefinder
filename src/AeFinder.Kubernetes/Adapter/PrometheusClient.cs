eb2e5e654ebf1fc480039e6e9de7dcf4957a8856
AeFinderBourne <astro@aefinder.io>
2024-11-08 19:16:36 +0800

    feat: optimize Prometheus Url

--- src/AeFinder.Kubernetes/Adapter/PrometheusClient.cs
+++ src/AeFinder.Kubernetes/Adapter/PrometheusClient.cs
@@ -1,20 +1,24 @@
+using AeFinder.Options;
+using Microsoft.Extensions.Options;
 using Volo.Abp.DependencyInjection;
 
 namespace AeFinder.Kubernetes.Adapter;
 
 public class PrometheusClient: IPrometheusClient, ISingletonDependency
 {
-    private readonly string prometheusBaseUrl = "http://prometheus.aelf.elf:30684";
+    private readonly KubernetesOptions _kubernetesOptions;
+    // private readonly string prometheusBaseUrl = "http://prometheus.aelf.elf:30684";
     
-    public PrometheusClient()
+    public PrometheusClient(IOptionsSnapshot<KubernetesOptions> kubernetesOptions)
     {
+        _kubernetesOptions = kubernetesOptions.Value;
     }
     
     public async Task<string> GetPodContainerCpuUsageInfoAsync(List<string> podNames)
     {
         // var query = $"sum(rate(container_cpu_usage_seconds_total{{namespace='{KubernetesConstants.AppNameSpace}', pod=~'{string.Join("|", podNames)}'}}[5m])) by (pod)";
         var query = $"sum(rate(container_cpu_usage_seconds_total{{namespace='{KubernetesConstants.AppNameSpace}', pod=~'{string.Join("|", podNames)}', container!='POD'}}[5m])) by (pod, container)";
-        string result = await QueryPrometheusAsync(prometheusBaseUrl, query);
+        string result = await QueryPrometheusAsync(query);
         return result;
     }
     
@@ -22,12 +26,13 @@ public class PrometheusClient: IPrometheusClient, ISingletonDependency
     {
         // var query = $"sum(container_memory_usage_bytes{{namespace='{KubernetesConstants.AppNameSpace}', pod=~'{string.Join("|", podNames)}'}}) by (pod)";
         var query = $"sum(container_memory_usage_bytes{{namespace='{KubernetesConstants.AppNameSpace}', pod=~'{string.Join("|", podNames)}', container!='POD'}}) by (pod, container)";
-        string result = await QueryPrometheusAsync(prometheusBaseUrl, query);
+        string result = await QueryPrometheusAsync(query);
         return result;
     }
     
-    private async Task<string> QueryPrometheusAsync(string prometheusBaseUrl, string query)
+    private async Task<string> QueryPrometheusAsync(string query)
     {
+        string prometheusBaseUrl = _kubernetesOptions.PrometheusUrl;
         using (var httpClient = new HttpClient())
         {
             var uri = $"{prometheusBaseUrl}/api/v1/query" +
