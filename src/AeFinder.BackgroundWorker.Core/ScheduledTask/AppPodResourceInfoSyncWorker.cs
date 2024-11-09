9bb7bebea98983a3912ad6e66e170bcf4f584877
AeFinderBourne <astro@aefinder.io>
2024-11-09 16:16:06 +0800

    feat: optimize pod list sync worker

--- src/AeFinder.BackgroundWorker.Core/ScheduledTask/AppPodResourceInfoSyncWorker.cs
+++ src/AeFinder.BackgroundWorker.Core/ScheduledTask/AppPodResourceInfoSyncWorker.cs
@@ -17,23 +17,19 @@ namespace AeFinder.BackgroundWorker.ScheduledTask;
 public class AppPodResourceInfoSyncWorker: AsyncPeriodicBackgroundWorkerBase, ISingletonDependency
 {
     private readonly ILogger<AppPodResourceInfoSyncWorker> _logger;
-    private readonly IAppDeployService _appDeployService;
     private readonly ScheduledTaskOptions _scheduledTaskOptions;
     private readonly IKubernetesAppMonitor _kubernetesAppMonitor;
-    private readonly IObjectMapper _objectMapper;
     private readonly IEntityMappingRepository<AppPodInfoIndex, string> _appPodInfoEntityMappingRepository;
     
     public AppPodResourceInfoSyncWorker(AbpAsyncTimer timer,ILogger<AppPodResourceInfoSyncWorker> logger, 
-        IKubernetesAppMonitor kubernetesAppMonitor,IObjectMapper objectMapper,
+        IKubernetesAppMonitor kubernetesAppMonitor,
         IEntityMappingRepository<AppPodInfoIndex, string> appPodInfoEntityMappingRepository,
-        IOptionsSnapshot<ScheduledTaskOptions> scheduledTaskOptions,IAppDeployService appDeployService,
+        IOptionsSnapshot<ScheduledTaskOptions> scheduledTaskOptions,
         IServiceScopeFactory serviceScopeFactory) : base(timer, serviceScopeFactory)
     {
         _logger = logger;
         _scheduledTaskOptions = scheduledTaskOptions.Value;
-        _appDeployService = appDeployService;
         _kubernetesAppMonitor = kubernetesAppMonitor;
-        _objectMapper = objectMapper;
         _appPodInfoEntityMappingRepository = appPodInfoEntityMappingRepository;
         // Timer.Period = 3 * 60 * 1000; // 180000 milliseconds = 3 minutes
         Timer.Period = _scheduledTaskOptions.AppPodResourceSyncTaskPeriodMilliSeconds;
