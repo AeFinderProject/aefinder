ef1d0b841ff1adc9d8576c0cce277ae2947ca17f
AeFinderBourne <astro@aefinder.io>
2024-12-23 18:17:05 +0800

    feat: add billing overview api

---
+++ src/AeFinder.Application.Contracts/Market/Dto/ApiQueryBillingOverviewDto.cs
@@ -0,0 +1,8 @@
+namespace AeFinder.Market;
+
+public class ApiQueryBillingOverviewDto
+{
+    public decimal ApiQueryLockedBalance { get; set; }
+    public decimal ApiQueryDailyCostAverage { get; set; }
+    public decimal ApiQueryMonthlyCostAverage { get; set; }
+}
