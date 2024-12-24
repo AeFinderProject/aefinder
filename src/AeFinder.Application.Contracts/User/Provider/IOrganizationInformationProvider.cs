c82d781ccbc3bf6fd7f27151478ed9fd8018c7f3
AeFinderBourne <astro@aefinder.io>
2024-12-24 15:00:38 +0800

    feat: get organization wallet address

--- src/AeFinder.Application.Contracts/User/Provider/IOrganizationInformationProvider.cs
+++ src/AeFinder.Application.Contracts/User/Provider/IOrganizationInformationProvider.cs
@@ -7,4 +7,5 @@ public interface IOrganizationInformationProvider
 {
     Task<string> GetUserOrganizationWalletAddressAsync(string organizationId, string userWalletAddress);
     Task<bool> SaveOrganizationExtensionInfoAsync(OrganizationExtensionDto organizationExtensionDto);
+    Task<string> GetOrganizationWalletAddressAsync(string organizationId);
 }
