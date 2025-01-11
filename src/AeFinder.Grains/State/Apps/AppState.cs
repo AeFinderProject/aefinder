8a2da37c0df02fcfba96dc9dd4f693940bad6240
sinclair <sinclair@aefinder.io>
2025-01-11 11:48:26 +0800

    fix: add default value

--- src/AeFinder.Grains/State/Apps/AppState.cs
+++ src/AeFinder.Grains/State/Apps/AppState.cs
@@ -17,5 +17,5 @@ public class AppState
     [Id(9)]public DateTime UpdateTime { get; set; }
     [Id(10)]public DateTime DeleteTime { get; set; }
     [Id(11)]public DateTime? DeployTime { get; set; }
-    [Id(12)] public bool IsLocked { get; set; }
+    [Id(12)] public bool IsLocked { get; set; } = false;
 }
