5df81d5cb92e93801f1b6a68c2642fef790b9c03
sinclair <sinclair@aefinder.io>
2025-01-20 14:37:23 +0800

    feat: improve asset init

--- src/AeFinder.Application/Assets/AssetInitializationService.cs
+++ src/AeFinder.Application/Assets/AssetInitializationService.cs
@@ -29,15 +29,18 @@ public class AssetInitializationService : AeFinderAppService, IAssetInitializati
     private readonly IMerchandiseService _merchandiseService;
     private readonly IClock _clock;
     private readonly AssetInitializationOptions _assetInitializationOptions;
+    private readonly IEntityMappingRepository<MerchandiseIndex, Guid> _merchandiseIndexRepository;
 
     public AssetInitializationService(IMerchandiseService merchandiseService, IAssetService assetService,
         IApiKeyService apiKeyService, IClock clock,
-        IOptionsSnapshot<AssetInitializationOptions> assetInitializationOptions)
+        IOptionsSnapshot<AssetInitializationOptions> assetInitializationOptions,
+        IEntityMappingRepository<MerchandiseIndex, Guid> merchandiseIndexRepository)
     {
         _merchandiseService = merchandiseService;
         _assetService = assetService;
         _apiKeyService = apiKeyService;
         _clock = clock;
+        _merchandiseIndexRepository = merchandiseIndexRepository;
         _assetInitializationOptions = assetInitializationOptions.Value;
     }
 
@@ -46,7 +49,9 @@ public class AssetInitializationService : AeFinderAppService, IAssetInitializati
         var time = _clock.Now;
         foreach (var item in _assetInitializationOptions.Assets)
         {
-            if (item.MerchandiseType == MerchandiseType.ApiQuery)
+            var merchandise = await _merchandiseIndexRepository.GetAsync(Guid.Parse(item.MerchandiseId));
+            
+            if (merchandise.Type == (int)MerchandiseType.ApiQuery)
             {
                 var existAssets = await _assetService.GetListAsync(organizationId, new GetAssetInput
                 {
@@ -58,15 +63,10 @@ public class AssetInitializationService : AeFinderAppService, IAssetInitializati
                     continue;
                 }
             }
-
-            var merchandise = await _merchandiseService.GetListAsync(new GetMerchandiseInput
-            {
-                Type = item.MerchandiseType
-            });
             
             var asset = await _assetService.CreateAsync(organizationId, new CreateAssetInput
             {
-                MerchandiseId = merchandise.Items.First().Id,
+                MerchandiseId = merchandise.Id,
                 Quantity = item.Quantity,
                 Replicas = item.Replicas,
                 FreeQuantity = item.FreeQuantity,
@@ -75,7 +75,7 @@ public class AssetInitializationService : AeFinderAppService, IAssetInitializati
                 CreateTime = time
             });
 
-            if (item.MerchandiseType == MerchandiseType.ApiQuery)
+            if (merchandise.Type == (int)MerchandiseType.ApiQuery)
             {
                 await _assetService.StartUsingAssetAsync(asset.Id, time);
                 await _apiKeyService.SetQueryLimitAsync(organizationId, item.Quantity);
