48b7f8740d09f4783dbf112b8274ed66ad73c7f8
sinclair <sinclair@aefinder.io>
2025-02-21 16:35:50 +0800

    feat: test unzip

--- src/AeFinder.Application/ZipHelper.cs
+++ src/AeFinder.Application/ZipHelper.cs
@@ -21,20 +21,9 @@ public class ZipHelper
         source.Seek(0, SeekOrigin.Begin);
         using (var s = new ZipInputStream(source))
         {
-            ZipEntry theEntry;
-            while ((theEntry = s.GetNextEntry()) != null)
-            {
-                if (!entryName.IsNullOrWhiteSpace() &&
-                    !string.Equals(theEntry.Name, entryName, StringComparison.OrdinalIgnoreCase))
-                {
-                    continue;
-                }
-
-                var buffer = new byte[4096];
-                StreamUtils.Copy(s, destination, buffer);
-                destination.Seek(0, SeekOrigin.Begin);
-                break;
-            }
+            var buffer = new byte[4096];
+            StreamUtils.Copy(s, destination, buffer);
+            destination.Seek(0, SeekOrigin.Begin);
         }
     }
 }
